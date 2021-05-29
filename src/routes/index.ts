c5f1da580fb6fa33489bca694ab5efab8ba41efd
Hercilio M. Ortiz <hercilio.ortiz@acad.pucrs.br>
2021-05-29 10:00:39 -0300

    Sprint 3 -> First Deploy

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,21 @@
 import express, { Request, Response } from 'express';
-import { addFundInfo, fundUpdate, getFunds } from '../controllers/fund';
+import {
+  addFundInfo,
+  fundUpdate,
+  getChart,
+  getFundDetails,
+  getFunds,
+} from '../controllers/fund';
 import authentication from '../middleware/authentication';
 
 const routes = express.Router();
 
+// Funcao inicial da aplicacao, somente testa se aplicacao ativa
 routes.get('/', (req: Request, res: Response) => {
   res.send('Hello world');
 });
 
+// Pesquisa por fundos no banco de dados, a partir de uma query param
 routes.get('/pesquisa', async (req: Request, res: Response) => {
   try {
     const param = req.query;
@@ -23,8 +31,42 @@ routes.get('/pesquisa', async (req: Request, res: Response) => {
   }
 });
 
+// Retorna o primeiro fundo encontrado a partir de um cnpj
+routes.get('/fundo/:cnpj', async (req: Request, res: Response) => {
+  try {
+    const { cnpj } = req.params;
+    const fundos = await getFundDetails(cnpj);
+    res.status(200).send(fundos);
+  } catch (e) {
+    res.send(400).send({
+      error: 'Failed to fetch fund',
+    });
+  }
+});
+
+routes.get('/rentabilidade', async (req: Request, res: Response) => {
+  try {
+    const { fundos, from, to } = req.query;
+    if (!fundos) {
+      res.status(400).send({ error: 'Error in API request' });
+      return;
+    }
+    const fundsChart = await getChart(
+      fundos as string[],
+      from as string | undefined,
+      to as string | undefined
+    );
+    res.send(fundsChart);
+  } catch (e) {
+    res.status(400).send({
+      error: 'Failed to create chart response',
+    });
+  }
+});
+
 routes.use(authentication);
 
+// Adiciona um novo fundo no banco de dados, passando um body json
 routes.post('/fundo', (req: Request, res: Response) => {
   try {
     addFundInfo(req.body);
@@ -36,6 +78,7 @@ routes.post('/fundo', (req: Request, res: Response) => {
   }
 });
 
+// Adiciona um novo fundo_update no banco de dados, passando um body json
 routes.post('/update', (req: Request, res: Response) => {
   try {
     fundUpdate(req.body);
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,24 @@
 import express from 'express';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import routes from './routes';
 
-const PORT = 4000;
+import swaggerFile from './swagger_output.json';
 
+const PORT = 4000;
 const app = express();
 
 const corsOptions: cors.CorsOptions = {
   origin: '*',
-  methods: "GET,HEAD,POST",
+  methods: 'GET,HEAD,POST',
 };
 
 app.use(cors(corsOptions));
 
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use('/api', routes);
 
